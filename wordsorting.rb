fa7caf089beb943ee84af098f52c135bab615bcb
Christian Chavarro <christian@clutchprep.com>
2016-04-23 14:14:32 -0400

    Word sorting exercise

--- wordsorting.rb
+++ wordsorting.rb
@@ -3,9 +3,6 @@ test = gets.chomp
 array = test.gsub(/\W/, ' ')
 
 input = array.split(" ")
+input = input.sort {|a,b| a.upcase <=> b.upcase}
 
-puts input
-
-input = input.sort {|a,b| a <=> b}
-
-puts input
+print input

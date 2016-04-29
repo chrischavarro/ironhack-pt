8f7824c9b7baf652f0c3ccf79275f2f4f4ad147e
Christian Chavarro <christian@clutchprep.com>
2016-04-28 20:09:43 -0400

    Renamed honk method to make_noise

--- classes/car.rb
+++ classes/car.rb
@@ -11,9 +11,12 @@
  # Price
 
 class Car # Capital letters msut only be used for constants or classes
-	def initialize(my_color, car_sound)
+	
+	attr_accessor :color
+
+	def initialize(my_color, make_noise)
 		@color = my_color
-		@sound = car_sound
+		@sound = make_noise
 	end
 	def honk # Instance method
 		puts "My car goes #{@sound}"
@@ -24,16 +27,26 @@ class Car # Capital letters msut only be used for constants or classes
 	end
 end
 
+class Racecar < Car
+	def honk
+		puts "My #{@color} car goes #{@sound.upcase}"
+	end
+end
+
 volvo = Car.new("Red", "Beep beep yo") # Everything within a bracket is parameter
 mazda = Car.new("Yellow", "Vrroom I'm a bitchin' Mazda")
 honda = Car.new("Orange", "I'm your grandma's favorite car")
 tesla = Car.new("Black", "Oh shit this guy's got money")
 escalade = Car.new("Silver", "I'M A BIG TIME BALLER, SHOT CALLER")
 
-volvo.honk
-mazda.honk
-honda.honk
-tesla.honk
-escalade.honk
+ferrari = Racecar.new("Red", "i'm an accident waiting to happen")
 
-tesla.play_radio
+puts ferrari.honk
+
+# volvo.honk
+# mazda.honk
+# honda.honk
+# tesla.honk
+# escalade.honk
+# tesla.play_radio
+# puts tesla.color

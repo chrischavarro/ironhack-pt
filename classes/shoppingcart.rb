10154878bfefb114f555045b8e29d94ff90f4819
Christian Chavarro <christian@clutchprep.com>
2016-04-30 11:02:07 -0400

    Shopping cart round 1

---
+++ classes/shoppingcart.rb
@@ -0,0 +1,65 @@
+class ShoppingCart
+	def initialize
+		@items = []
+	end
+
+	def add_to_cart(item)
+		@items.push(item)
+	end
+
+	def checkout
+		total = 0
+
+		@items.each do |single_item|
+			total += single_item.price
+		end
+		total
+	end
+end
+
+class Items
+	def initialize(name,price)
+		@name = name
+		@price = price
+	end
+
+	def price
+		@price
+	end
+end
+
+class HouseWare < Item
+	def price
+		if @price > 100
+			@price * 0.95
+		else
+			@price
+		end
+	end
+end
+
+class Fruit < Item
+	def price
+		today = Time.now
+
+		if today.saturday? || today.sunday?
+			@price * 0.90 # If you want to get rid of .0, put this in parenthesis and insert .round
+		else
+			@price
+	end
+end
+
+banana = Fruit.new("Banana", 10)
+vacuum = HouseWare.new("Vacuum Cleaner", 150)
+
+orange_juice = Item.new("Orange Juice", 10)
+rice = Item.new("Rice", 1)
+anchovies = Item.new("Anchovies", 2)
+
+my_cart = ShoppingCart.new
+my_cart.add_to_cart(banana)
+my_cart.add_to_cart(banana)
+my_cart.add_to_cart(vacuum)
+my_cart.add_to_cart(rice)
+
+puts "Your total today is $#{my_cart.checkout}"
